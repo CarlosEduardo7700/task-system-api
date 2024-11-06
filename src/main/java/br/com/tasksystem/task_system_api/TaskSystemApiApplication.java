9c92f97b4991d301cc6365fd32df40e4d91f8859
CarlosEduardo7700 <rm552164@fiap.com.br>
2024-11-05 21:23:12 -0300

    Enviando para o GitHub

---
+++ src/main/java/br/com/tasksystem/task_system_api/TaskSystemApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.tasksystem.task_system_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskSystemApiApplication.class, args);
+	}
+
+}
